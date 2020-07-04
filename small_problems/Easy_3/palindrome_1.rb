fa8f11daebaec0fcb45a3e53a5a9d3bb76d60d74
karlwitek <karl.witek20@gmail.com>
2020-07-04 15:31:32 -0700

    complete palindrome part 1

---
+++ small_problems/Easy_3/palindrome_1.rb
@@ -0,0 +1,44 @@
+# Write a method that returns true if the string passed as and argument
+# is a palindrome, false otherwise.  A palindrome reads the same forward
+# or backward. For this exercise, case matters as does punctuation
+# and spaces.
+
+def is_palindrome(string)
+  if string == string.reverse
+    true
+  else 
+    false
+  end
+end
+
+puts is_palindrome('abba')
+puts is_palindrome('abba ')
+
+# works.  LS below
+
+def palindrome?(string)
+  string == string.reverse
+end
+
+puts palindrome?('madam')
+
+## Further Exploration
+
+# Write a method that determines whether an array is palindromic.
+# ( elements appear in the same sequence both forwards and backwards)
+# Second, write a method that can take either an array or a string 
+# argument, and determines whether that argument is a palindrome.
+# Don't use an if, unless, or case statement or modifier.
+
+def palindrome_array(array)
+  array == array.reverse
+end
+
+puts '-----------------'
+puts palindrome_array([2, 2, 4, 2, 2])
+puts palindrome_array('not one')
+puts palindrome_array('madam')
+
+# Done. Ruby interprets the argument. An array or string
+# is accepted.
+
