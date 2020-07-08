316623923d881dced2175e087b35c66b37b7b4ea
karlwitek <karl.witek20@gmail.com>
2020-07-08 11:09:22 -0700

    debugging convert

--- small_problems/Easy_4/convert_string.rb
+++ small_problems/Easy_4/convert_string.rb
@@ -8,16 +8,6 @@
 # p hash
 
 # def strint_to_integer(num_string)
-#   digit = 0
-#   spl_array = num_string.split('')
-#   p spl_array
-#   spl_array.each do |num|
-#     hash[num] = digit
-#     puts digit
-#   end
-# end
-
-# string_to_integer('12')
 
 #  NOT SOLVED.  LS below
 
@@ -25,11 +15,15 @@ hash = { '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5,
         '6' => 6, '7' => 7, '8' => 8, '9' => 9 }
 
 
+# string = '123'
+# dig = string.chars.map { |char| hash[char] }
+# p dig
+
 def string_to_integer(string)
   digits = string.chars.map { |char| hash[char] }
   
   value = 0
-  digits.each { |digit| value = 10 * value + digit}
+  digits.each { |digit| value = 10 * value + digit }
   value
 end
 
