d27020a3840d3b2b4be2315a2147e52f82202a0f
karlwitek <karl.witek20@gmail.com>
2020-07-24 16:44:49 -0700

    problem 3

---
+++ lesson4/practice_problems3.rb
@@ -0,0 +1,42 @@
+# Remove people with age 100 or greater from the hash:
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+# ages.delete_if do |_, age|
+#   age >= 100
+# end
+
+# p ages
+# works.  try another solution
+# h = ages.map do |name, age|
+#   if age >= 100 
+#     ages.delete(name)
+#     ages.delete(age)
+#   else
+#     ages[name] = age
+#   end
+# end
+
+# not correct lines 10 - 18
+# ages.delete 
+#   if age >= 100
+#     ages.delete(name)
+#   end
+# end
+# p ages
+# not correct.. LS -->
+
+# ages.keep_if { |_, age| age < 100 }
+
+# p ages
+# could also use .select! here.  Docs tell us that Hash#select! is 
+# equivalent to Hash#keep_if, but returns nil if no changes were made. 
+# In this case, would not have made any difference.
+
+# ages.select! { |_, age| age < 100 }
+
+# p ages
+# my solution on one line
+ages.delete_if { |_, age| age >= 100 }
+p ages
+
