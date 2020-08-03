fc7b3db81fa4d445d92dfc9c0301cb9af6ad6c62
karlwitek <karl.witek20@gmail.com>
2020-08-03 11:57:24 -0700

    problem 2 values in a hash

---
+++ lesson5/practice_prob2.rb
@@ -0,0 +1,29 @@
+# order this array of hashes based on the year of publication of each
+# book, from earliest to the latest.
+
+
+
+books = [
+  {title: 'One Hundred Years of Solitude', author: 'Gabriel Garcia Marquez', published: '1967'},
+  {title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', published: '1925'},
+  {title: 'War and Peace', author: 'Leo Tolstoy', published: '1869'},
+  {title: 'Ulysses', author: 'James Joyce', published: '1922'}
+]
+
+p books.sort_by { |hash| hash[:published].to_i }
+# works. same results without .to_i
+# LS -->
+
+b = books.sort_by do |book|
+    book[:published]
+  end
+
+p b
+# added 'b' to store return value and output.  We can use sort_by and access a 
+# particular value in each hash by which to sort, as long as those values have 
+# a <=> method.  In this case, all the values for :published are strings, so we
+# know that we can compare them.  Since all the values in question are four
+# characters in length, we can simply compare the strings without having to covert
+# them to integers.
+
+
