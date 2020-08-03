f1feb1e3edf5cf818adae3368ecedc057c6a6cb5
karlwitek <karl.witek20@gmail.com>
2020-08-03 13:53:52 -0700

    add comment

--- lesson5/practice_prob7.rb
+++ lesson5/practice_prob7.rb
@@ -14,7 +14,7 @@ p arr  # [4, [3, 8]]
 # The value of a didn't change because we are not referencing a at any point.
 # This code arr[0] += 2 was modifying the array, arr not a.  In effect we are
 # assigning a new object at that index of the array so that instead of arr[0] 
-# containing a it now contains 4.  The value of b did change because b is an 
+# containing a, it now contains 4.  The value of b did change because b is an 
 # array and we are modifying that array by assigning a new value at index 0 of
 # that array.
 
