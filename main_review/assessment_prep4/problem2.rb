f6627fcac9c9da7c88fca557017317e82a7b89d0
karlwitek <karl.witek20@gmail.com>
2020-10-09 11:55:01 -0700

    second assessment problem

---
+++ main_review/assessment_prep4/problem2.rb
@@ -0,0 +1,40 @@
+# Second problem from assessment.  Given a string of one or more words, find the letter that has the
+# most occurrences in the string and return that letter.  If two or more letters have the same
+# (largest number of occurrences), return the first letter in the string with that many occurrences.
+
+# input - string of one or more words
+# output - the letter with the most occurrences
+
+# split string into an array of characters
+# initialize a hash keys=letters, values = number of occurrences
+# iterate over array of characters
+# assign key/value pairs to hash while using #count called on string
+# find key/value(s) with highest value
+# call #each on hash with block that finds the key with the highest value (use > not >= to find first)
+# return the key of the highest value 
+
+# (hash will return the first key if two values are the same) if using method on hash may not use
+# this step
+
+def find_letter_of_most_occurences(string)
+  hash = Hash.new(0)
+  char_array = string.chars
+
+  char_array.each do |letter|
+    hash[letter] = string.count(letter)
+  end
+  max_value = hash.values.max
+
+  hash.each do |key, value|
+    if value == max_value
+      return key
+    end
+  end
+end
+
+str = 'accddeefffffgg hijjjjjkllmno'
+
+p find_letter_of_most_occurences(str) # f
+
+
+
